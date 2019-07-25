842fa5bfe4234382df89305a940c5ea9d8358ea5
Alex Siman <aleksandr.siman@gmail.com>
2019-07-25 19:52:21 +0300

    Add CLI command: Find available test scenarios

--- src/cli.ts
+++ src/cli.ts
@@ -1,5 +1,7 @@
 import { runTestFile } from "./run-test-file";
-import { arrayToConsoleString } from './utils';
+import { arrayToConsoleString, TEST_SCENARIOS_FOLDER } from './utils';
+import * as fs from 'fs';
+import * as path from 'path';
 
 const program = require('commander');
 
@@ -12,8 +14,22 @@ const testsText = `<tests> is a comma separated list of test files.`
 program
   .version('0.1.0')
   .description('Storage Benchmarking Tool for Joystream Network')
+  .option('-l, --list', `Lookup available test scenario(s).`)
   .option('-t, --test <tests>', `Run test scenario(s). ${testsText}`, commaSeparatedList)
   .parse(process.argv);
+
+if (program.list) {
+  // tslint:disable-next-line:non-literal-fs-path
+  const testFileNames = fs.readdirSync(TEST_SCENARIOS_FOLDER).filter(file => {
+    const filePath = path.join(TEST_SCENARIOS_FOLDER, file)
+    const isJsOrJsonFile = file.endsWith(`.js`) || file.endsWith(`.json`)
+    // tslint:disable-next-line:non-literal-fs-path
+    return isJsOrJsonFile && fs.statSync(filePath).isFile();
+  });
+  console.log(`Found ${testFileNames.length} test scenario(s):`)
+  console.log(arrayToConsoleString(testFileNames))
+  process.exit(0)
+}
 
 const testFileNames = program.test as string[];
 if (!testFileNames || testFileNames.length === 0) {
